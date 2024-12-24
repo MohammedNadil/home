c6dad9e73bf26e9a899348b3968f682f1f516770
Nadil <mohammednadil5@gmail.com>
2024-12-24 18:44:10 +0530

    test

--- index.js
+++ index.js
@@ -145,6 +145,17 @@
     showSceneList();
   }
 
+  // Set handler for scene switch.
+  scenes.forEach(function(scene) {
+    var el = document.querySelector('#sceneList .scene[data-id="' + scene.data.id + '"]');
+    el.addEventListener('click', function() {
+      switchScene(scene);
+      // On mobile, hide scene list after selecting a scene.
+      if (document.body.classList.contains('mobile')) {
+        hideSceneList();
+      }
+    });
+  });
 
   // DOM elements for view controls.
   var viewUpElement = document.querySelector('#viewUp');
